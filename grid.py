fca9c7e72844cf7706bf9945576ecd21512e4753
Maleke <malekelthomas97@gmail.com>
2020-06-14 17:12:40 -0400

    Added some a star algo code

--- grid.py
+++ grid.py
@@ -1,16 +1,45 @@
-grid = []
+import math
+def distance(start_point, end_point):
+	x1 = start_point[0]
+	x2 = end_point[0]
+	y1 = start_point[1]
+	y2 = end_point[1]
+	
+	return int(10*math.sqrt((x2-x1)**2+(y2-y1)**2))
 
-for i in range(13):
-	for j in range(20):
-		grid.append([105*i,105*j])
+closed =[]
+visiting = []
+path = []
 
-
-abs_diff_func = lambda list_val, given_val : abs(list_val-given_val)
-
-
-
-x_coords = [i[0] for i in grid]
-y_coords = [i[1] for i in grid]
-
-closest_val = min(x_coords, key=abs_diff_func(1))
-print(closest_val)
+print(distance([0,0],[1,1]))
+def a_star(origin, end, grid):
+	for block in grid:
+		if block.coords == origin:
+			block.path_weight = 0
+			block.heuristic = distance(origin,end)
+			visiting.append(block)
+		if distance(block.coords, origin) <=14:
+			block.path_weight = distance(block.coords, origin)
+			block.heuristic = block.path_weight+distance(block.coords, end)
+			visiting.append(block)
+		else:
+			closed.append(visiting[0].pop())
+			path.append(closed[0])
+			min_heuristic = path[0].heuristic
+			for visit in visiting:
+				if visit.heurisitic < min_heuristic:
+					min_heuristic = visit.heuristic
+					if distance(block.coords, visit) <= 14:
+						block.path_weight+=visit.path_weight
+						block.heuristic = block.path_weight+distance(block.coords,end)
+			
+			
+		
+			
+			
+		
+		
+		
+	
+	
+	
