a72c149b27a57b97ac4381a5b2e5eec889347989
Maleke <malekelthomas97@gmail.com>
2020-06-18 15:44:53 -0400

    Fixed

--- test.py
+++ test.py
@@ -6,29 +6,45 @@ class Block():
 	
 	def __init__(self):
 		self.coords = []
-		self.status = "unvisited"
+		self.status = "blank"
 		self.color = (137,125,125)
-		self.path_weight = 0
-		self.heuristic = 0
+		self.g_cost = 0
+		self.h_cost = 0
+		self.f_cost = 0
+		self.point_came_from = []
 	
 	def set_status(self, status):
 		self.status = status
 	
-	def set_color(self, color):
-		self.color = color
-		
+	def set_color(self):
+		if self.status == "origin":
+			self.color == (128,0,128)
+		if self.status == "end":
+			self.color == (128,0,128)
+		elif self.status == "unvisited":
+			self.color = (255,255,255)
+		elif self.status == "visited":
+			self.color = (33,208,196)
+		elif self.status == "closed":
+			self.color = (208,33,33)
+		elif self.status == "path":
+			self.color = (57,208,33)
+	
 	def set_coords(self, x, y):
 		self.coords.append(x)
 		self.coords.append(y)
 	
-	def set_path_weight(self, path_weight):
-		self.path_weight = path_weight
+	def set_g_cost(self, g_cost):
+		self.g_cost = g_cost
 	
-	def set_heuristic(self, heuristic):
-		self.heuristic = heuristic
+	def set_h_cost(self, h_cost):
+		self.h_cost = h_cost
+
+	def set_f_cost(self):
+		self.f_cost = self.g_cost+self.h_cost
 	
-	def get_heuristic(self):
-		return self.heuristic
+	def get_f_cost(self):
+		return self.f_cost
 		
 def distance(start_point, end_point):
 	x1 = start_point[0]
@@ -41,8 +57,9 @@ def distance(start_point, end_point):
 grid = {}
 origin = [105,105]
 end = [840,840]
-visiting = []
-closed =[]
+seen_unvisited= []
+seen_visited =[]
+closed = []
 
 for i in range(13):
 	for j in range(20):
@@ -53,107 +70,203 @@ for i in range(13):
 
 for point in grid:
 	if list(point) == origin:
-		grid[point].set_heuristic(distance(origin,end))
-		grid[point].set_color((128,0,128))
-		grid[point].set_status("visited")
-		visiting.append(grid[point])
+		grid[point].set_h_cost(distance(origin,end))
+		grid[point].set_f_cost()
+		grid[point].set_status("origin")
+		grid[point].set_color()
+		seen_visited.append(grid[point])
 
 while(True):
-	if any(x.coords == end for x in visiting):
-		break
 	
-	current_coords = visiting[0].coords
-	print("cur", current_coords)
+	current_coords = seen_visited[0].coords
+	print("cur", current_coords, grid[tuple(current_coords)].get_f_cost())
 	for point in grid:
-		if grid[point] in visiting:
+		if grid[point] in zip(seen_visited,closed):
 			continue
 		if grid[point].coords[0] == current_coords[0]+105 and grid[point].coords[1] == current_coords[1]:
-			if current_coords == origin:
-				grid[point].set_path_weight(distance(point, current_coords))
+			if list(point) == end:
+				grid[point].set_g_cost(seen_visited[0].g_cost+distance(point,current_coords))
+				grid[point].set_h_cost(distance(point, end))
+				grid[point].set_f_cost()
+				grid[point].set_status("end")
+				grid[point].set_color()
+				grid[tuple(current_coords)].set_status("closed")
+				grid[tuple(current_coords)].set_color()
+				closed.append(grid[tuple(current_coords)])
+				closed.append(grid[point])
+				break
+			elif current_coords == origin:
+				grid[point].set_g_cost(distance(point, current_coords))
 			else:
-				grid[point].set_path_weight(visiting[0].path_weight+distance(point,current_coords))
-			grid[point].set_heuristic(distance(point, end)+grid[point].path_weight)
-			grid[point].set_status("visited")
-			grid[point].set_color((128,0,128))
-			visiting.append(grid[point])
+				grid[point].set_g_cost(seen_visited[0].g_cost+distance(point,current_coords))
+			grid[point].set_h_cost(distance(point, end))
+			grid[point].set_f_cost()
+			grid[point].set_status("unvisited")
+			grid[point].set_color()
+			seen_unvisited.append(grid[point])
 		elif grid[point].coords[0] == current_coords[0]-105 and grid[point].coords[1] == current_coords[1]:
-			if current_coords == origin:
-				grid[point].set_path_weight(distance(point, current_coords))
+			if list(point) == end:
+				grid[point].set_g_cost(seen_visited[0].g_cost+distance(point,current_coords))
+				grid[point].set_h_cost(distance(point, end))
+				grid[point].set_f_cost()
+				grid[point].set_status("end")
+				grid[point].set_color()
+				grid[tuple(current_coords)].set_status("closed")
+				grid[tuple(current_coords)].set_color()
+				closed.append(grid[tuple(current_coords)])
+				closed.append(grid[point])
+				break
+			elif current_coords == origin:
+				grid[point].set_g_cost(distance(point, current_coords))
 			else:
-				grid[point].set_path_weight(visiting[0].path_weight+distance(point,current_coords))
-			grid[point].set_heuristic(distance(point, end)+grid[point].path_weight)
-			grid[point].set_status("visited")
-			grid[point].set_color((128,0,128))
-			visiting.append(grid[point])
+				grid[point].set_g_cost(seen_visited[0].g_cost+distance(point,current_coords))
+			grid[point].set_h_cost(distance(point, end))
+			grid[point].set_f_cost()
+			grid[point].set_status("unvisited")
+			grid[point].set_color()
+			seen_unvisited.append(grid[point])
 		elif grid[point].coords[0] == current_coords[0] and grid[point].coords[1] == current_coords[1]+105:
-			if current_coords == origin:
-				grid[point].set_path_weight(distance(point, current_coords))
+			if list(point) == end:
+				grid[point].set_g_cost(seen_visited[0].g_cost+distance(point,current_coords))
+				grid[point].set_h_cost(distance(point, end))
+				grid[point].set_f_cost()
+				grid[point].set_status("end")
+				grid[point].set_color()
+				grid[tuple(current_coords)].set_status("closed")
+				grid[tuple(current_coords)].set_color()
+				closed.append(grid[tuple(current_coords)])
+				closed.append(grid[point])
+				break
+			elif current_coords == origin:
+				grid[point].set_g_cost(distance(point, current_coords))
 			else:
-				grid[point].set_path_weight(visiting[0].path_weight+distance(point,current_coords))
-			grid[point].set_heuristic(distance(point, end)+grid[point].path_weight)
-			grid[point].set_status("visited")
-			grid[point].set_color((128,0,128))
-			visiting.append(grid[point])
+				grid[point].set_g_cost(seen_visited[0].g_cost+distance(point,current_coords))
+			grid[point].set_h_cost(distance(point, end))
+			grid[point].set_f_cost()
+			grid[point].set_status("unvisited")
+			grid[point].set_color()
+			seen_unvisited.append(grid[point])
 		elif grid[point].coords[0] == current_coords[0] and grid[point].coords[1] == current_coords[1]-105:
-			if current_coords == origin:
-				grid[point].set_path_weight(distance(point, current_coords))
+			if list(point) == end:
+				grid[point].set_g_cost(seen_visited[0].g_cost+distance(point,current_coords))
+				grid[point].set_h_cost(distance(point, end))
+				grid[point].set_f_cost()
+				grid[point].set_status("end")
+				grid[point].set_color()
+				grid[tuple(current_coords)].set_status("closed")
+				grid[tuple(current_coords)].set_color()
+				closed.append(grid[tuple(current_coords)])
+				closed.append(grid[point])
+				break
+			elif current_coords == origin:
+				grid[point].set_g_cost(distance(point, current_coords))
 			else:
-				grid[point].set_path_weight(visiting[0].path_weight+distance(point,current_coords))
-			grid[point].set_heuristic(distance(point, end)+grid[point].path_weight)
-			grid[point].set_status("visited")
-			grid[point].set_color((128,0,128))
-			visiting.append(grid[point])
+				grid[point].set_g_cost(seen_visited[0].g_cost+distance(point,current_coords))
+			grid[point].set_h_cost(distance(point, end))
+			grid[point].set_f_cost()
+			grid[point].set_status("unvisited")
+			grid[point].set_color()
+			seen_unvisited.append(grid[point])
 		elif grid[point].coords[0] == current_coords[0]-105 and grid[point].coords[1] == current_coords[1]-105:
-			if current_coords == origin:
-				grid[point].set_path_weight(distance(point, current_coords))
+			if list(point) == end:
+				grid[point].set_g_cost(seen_visited[0].g_cost+distance(point,current_coords))
+				grid[point].set_h_cost(distance(point, end))
+				grid[point].set_f_cost()
+				grid[point].set_status("end")
+				grid[point].set_color()
+				grid[tuple(current_coords)].set_status("closed")
+				grid[tuple(current_coords)].set_color()
+				closed.append(grid[tuple(current_coords)])
+				closed.append(grid[point])
+				break
+			elif current_coords == origin:
+				grid[point].set_g_cost(distance(point, current_coords))
 			else:
-				grid[point].set_path_weight(visiting[0].path_weight+distance(point,current_coords))
-			grid[point].set_heuristic(distance(point, end)+grid[point].path_weight)
-			grid[point].set_status("visited")
-			grid[point].set_color((128,0,128))
-			visiting.append(grid[point])
+				grid[point].set_g_cost(seen_visited[0].g_cost+distance(point,current_coords))
+			grid[point].set_h_cost(distance(point, end))
+			grid[point].set_f_cost()
+			grid[point].set_status("unvisited")
+			grid[point].set_color()
+			seen_unvisited.append(grid[point])
 		elif grid[point].coords[0] == current_coords[0]+105 and grid[point].coords[1] == current_coords[1]-105:
-			if current_coords == origin:
-				grid[point].set_path_weight(distance(point, current_coords))
+			if list(point) == end:
+				grid[point].set_g_cost(seen_visited[0].g_cost+distance(point,current_coords))
+				grid[point].set_h_cost(distance(point, end))
+				grid[point].set_f_cost()
+				grid[point].set_status("end")
+				grid[point].set_color()
+				grid[tuple(current_coords)].set_status("closed")
+				grid[tuple(current_coords)].set_color()
+				closed.append(grid[tuple(current_coords)])
+				closed.append(grid[point])
+				break
+			elif current_coords == origin:
+				grid[point].set_g_cost(distance(point, current_coords))
 			else:
-				grid[point].set_path_weight(visiting[0].path_weight+distance(point,current_coords))
-			grid[point].set_heuristic(distance(point, end)+grid[point].path_weight)
-			grid[point].set_status("visited")
-			grid[point].set_color((128,0,128))
-			visiting.append(grid[point])
+				grid[point].set_g_cost(seen_visited[0].g_cost+distance(point,current_coords))
+			grid[point].set_h_cost(distance(point, end))
+			grid[point].set_f_cost()
+			grid[point].set_status("unvisited")
+			grid[point].set_color()
+			seen_unvisited.append(grid[point])
 		elif grid[point].coords[0] == current_coords[0]-105 and grid[point].coords[1] == current_coords[1]+105:
-			if current_coords == origin:
-				grid[point].set_path_weight(distance(point, current_coords))
+			if list(point) == end:
+				grid[point].set_g_cost(seen_visited[0].g_cost+distance(point,current_coords))
+				grid[point].set_h_cost(distance(point, end))
+				grid[point].set_f_cost()
+				grid[point].set_status("end")
+				grid[point].set_color()
+				grid[tuple(current_coords)].set_status("closed")
+				grid[tuple(current_coords)].set_color()
+				closed.append(grid[tuple(current_coords)])
+				closed.append(grid[point])
+				break
+			elif current_coords == origin:
+				grid[point].set_g_cost(distance(point, current_coords))
 			else:
-				grid[point].set_path_weight(visiting[0].path_weight+distance(point,current_coords))
-			grid[point].set_heuristic(distance(point, end)+grid[point].path_weight)
-			grid[point].set_status("visited")
-			grid[point].set_color((128,0,128))
-			visiting.append(grid[point])
+				grid[point].set_g_cost(seen_visited[0].g_cost+distance(point,current_coords))
+			grid[point].set_h_cost(distance(point, end))
+			grid[point].set_f_cost()
+			grid[point].set_status("unvisited")
+			grid[point].set_color()
+			seen_unvisited.append(grid[point])
 		elif grid[point].coords[0] == current_coords[0]+105 and grid[point].coords[1] == current_coords[1]+105:
-			if current_coords == origin:
-				grid[point].set_path_weight(distance(point, current_coords))
+			if list(point) == end:
+				grid[point].set_g_cost(seen_visited[0].g_cost+distance(point,current_coords))
+				grid[point].set_h_cost(distance(point, end))
+				grid[point].set_f_cost()
+				grid[point].set_status("end")
+				grid[point].set_color()
+				grid[tuple(current_coords)].set_status("closed")
+				grid[tuple(current_coords)].set_color()
+				closed.append(grid[tuple(current_coords)])
+				closed.append(grid[point])
+				break
+			elif current_coords == origin:
+				grid[point].set_g_cost(distance(point, current_coords))
 			else:
-				grid[point].set_path_weight(visiting[0].path_weight+distance(point,current_coords))
-			grid[point].set_heuristic(distance(point, end)+grid[point].path_weight)
-			grid[point].set_status("visited")
-			grid[point].set_color((128,0,128))
-			visiting.append(grid[point])
+				grid[point].set_g_cost(seen_visited[0].g_cost+distance(point,current_coords))
+			grid[point].set_h_cost(distance(point, end))
+			grid[point].set_f_cost()
+			grid[point].set_status("unvisited")
+			grid[point].set_color()
+			seen_unvisited.append(grid[point])
 	
-	if visiting[0] not in closed:
-		visiting[0].set_color((54,43,215))
-		visiting[0].set_status("closed")
-		closed.append(visiting.pop(0))
+	if grid[tuple(end)] in closed:
+		break
+
+	seen_visited[0].set_status("closed")
+	seen_visited[0].set_color()	
+	closed.append(seen_visited.pop(0))
 	
-	for i in closed:
+	for i in seen_unvisited:
 		print(i.coords, i.status)
-	print("-----------")
-	visiting.sort(key=lambda block:block.get_heuristic())
-		
+	print("-----------sorted")
+	seen_unvisited.sort(key=lambda point:point.get_f_cost())
+	for i in seen_unvisited:
+		print(i.coords, i.status)
+	print("-----------------")
+	seen_unvisited[0].set_status("visited")
+	seen_unvisited[0].set_color()
+	seen_visited.append(seen_unvisited.pop(0))
 
-for i in closed:
-	print(i.status, i.coords, i.path_weight,i.get_heuristic(), distance(i.coords, end))
-
-print("closed---------")
-for i in closed:
-	print(i.status, i.coords, i.path_weight, i.get_heuristic(), distance(i.coords, end))
